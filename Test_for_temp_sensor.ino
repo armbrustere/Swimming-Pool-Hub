b1b5c87f10d65a7618bd9b66db2fc5686d596eec
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-30 12:01:17 -0500

    Initial Commit for Pool Sensor Hub

---
+++ Test_for_temp_sensor.ino
@@ -0,0 +1,19 @@
+//Author: Ethan Armbruster
+//Date: 11/30/21
+//Rev 0.1 Swimming Pool Sensor "Hub"
+
+#include <WiFi101.h>
+#include <HttpClient.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
