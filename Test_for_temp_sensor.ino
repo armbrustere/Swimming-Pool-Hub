73fec4f0ed16988ee5a16c344b0e0d8e154e3d8f
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-30 13:49:11 -0500

    Begins to add the necessary commands to receive data from the Temperature Sensor.

--- Test_for_temp_sensor.ino
+++ Test_for_temp_sensor.ino
@@ -1,19 +1,48 @@
 //Author: Ethan Armbruster
 //Date: 11/30/21
 //Rev 0.1 Swimming Pool Sensor "Hub"
-
+#include <Arduino.h>
 #include <WiFi101.h>
 #include <HttpClient.h>
+#include <OneWire.h>
 
+int IOExShield_Pin_TempSensor = 2; //Temp Sensor is on Pin2 of the Extension Shield
 
+//Temp Chip I/O
+OneWire ts(IOExShield_Pin_TempSensor);
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
     Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    float temparture = getTemp();
+    Serial.println(temparture);
+
+    delay(1000);
+}
+
+
+//Returns the temparture of the sensor in Degrees Celsius
+float getTemp() {
+    byte data[12];
+    byte addr[8];
+
+    //No Sensors found on chain, reset search
+    if (!ts.search(addr)) {
+        ts.reset_search();
+        return -1000;
+    }
+
+    //Detects if the 8 bit CRC number from the sensor is present/valid
+    //This number is located in the ROM and registers of "the sensor"?
+    if(OneWire::crc8(addr,7) != addr[7])
+    {
+        Serial.println("CRC is not valid!");
+        return -1000;
+    }
 
 
 }
+
