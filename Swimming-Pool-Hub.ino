da1b9634640b829d9d4be9ab9f7afdb2f84303b2
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-30 17:13:13 -0500

    Finally works after just using the example code from the library not the retailer...

--- Swimming-Pool-Hub.ino
+++ Swimming-Pool-Hub.ino
@@ -6,43 +6,98 @@
 #include <HttpClient.h>
 #include <OneWire.h>
 
-int IOExShield_Pin_TempSensor = 2; //Temp Sensor is on Pin2 of the Extension Shield
+int IOExShield_Pin_TempSensor = 12; //Temp Sensor is on Pin2 of the Extension Shield
 
 //Temp Chip I/O
-OneWire ts(IOExShield_Pin_TempSensor);
+OneWire ds(IOExShield_Pin_TempSensor);
 
-void setup() {
+void setup(void) {
     // put your setup code here, to run once:
     Serial.begin(9600);
 }
 
-void loop() {
-    float temparture = getTemp();
-    Serial.println(temparture);
-
-    delay(1000);
+void loop(void) {
+    getTemp();
 }
 
 
 //Returns the temparture of the sensor in Degrees Celsius
 float getTemp() {
-    byte data[12];
-    byte addr[8];
+byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
 
-    //No Sensors found on chain, reset search
-    if (!ts.search(addr)) {
-        ts.reset_search();
-        return -1000;
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
     }
-
-    //Detects if the 8 bit CRC number from the sensor is present/valid
-    //This number is located in the ROM and registers of "the sensor"?
-    if(OneWire::crc8(addr,7) != addr[7])
-    {
-        Serial.println("CRC is not valid!");
-        return -1000;
-    }
-
-
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
 }
-
