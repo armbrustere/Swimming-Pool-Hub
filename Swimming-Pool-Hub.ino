153b1a05121a644c3b6a833728779ad4c0ad53ea
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-12-02 15:07:52 -0500

    Adds the ability to send temperature readings to a remote server, will eventually have it send to my SQL server at AWS.

--- Swimming-Pool-Hub.ino
+++ Swimming-Pool-Hub.ino
@@ -1,103 +1,226 @@
 //Author: Ethan Armbruster
 //Date: 11/30/21
 //Rev 0.1 Swimming Pool Sensor "Hub"
-#include <Arduino.h>
-#include <WiFi101.h>
-#include <HttpClient.h>
+
+//Libraries for WiFi Connectivity
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
+#include <ArduinoHttpClient.h>
+#include <ArduinoJson.h>
+
+
+
+//Temp Sensor Library
 #include <OneWire.h>
 
-int IOExShield_Pin_TempSensor = 12; //Temp Sensor is on Pin2 of the Extension Shield
+//PH Sensor Library
+#include <DFRobot_PH.h>
+#include <EEPROM.h>
 
-//Temp Chip I/O
-OneWire ds(IOExShield_Pin_TempSensor);
+//Data for WIFI Network Connection
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
 
-void setup(void) {
-    // put your setup code here, to run once:
+//Creates WiFiClient object
+WiFiClient client;
+
+//Temp Sensor is on Pin13 of the Extension Shield
+int IOExShield_Pin_TempSensor = 12;
+
+//Server Connected To
+char serverName[] = "www.ptsv2.com";
+char pageName[] = "/t/sensor/post";
+int serverPort = 80;
+
+//Time Variables for keeping track of Time Interval
+unsigned long thisMillis = 0;
+unsigned long lastMillis = 0;
+
+//Set Size of postBuffer to ensure little garbage is sent
+char bodyBuff[420];
+
+//Sets timer for sending new data
+#define delayTime 10000
+
+//Temp Chip I/O, tells OneWire what sensor to connect to
+OneWire ts(IOExShield_Pin_TempSensor);
+StaticJsonDocument<400> doc;
+
+
+void setup() {
     Serial.begin(9600);
+    attemptConnection();
 }
 
-void loop(void) {
+void loop() {
     getTemp();
+    catJSON(getTemp());
+    //catJSON();
+    delay(delayTime);
 }
 
 
-//Returns the temparture of the sensor in Degrees Celsius
+//Returns the temparture of the sensor in Degrees Celsius and Farenheit
 float getTemp() {
-byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
-  
-  if ( !ds.search(addr)) {
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
+    byte data[12];
+    byte addr[8];
 
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
- 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
+    //No Sensors found on chain, reset search
+    if (!ts.search(addr)) {
+        Serial.println("No Sensors Found!");
+        ts.reset_search();
+        return -1000;
     }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
+
+    //Detects if the 8 bit CRC number from the sensor is present/valid
+    //This number is located in the ROM and registers of "the sensor"?
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!");
+        return -1000;
+    }
+
+    if (addr[0] != 0x10 && addr[0] != 0x28) {
+        Serial.println("Device is not recognized");
+        return -1000;
+    }
+
+    ts.reset();
+    ts.select(addr);
+    ts.write(0x44, 1); //Starts the conversion with parasite power on at the end.
+
+    byte present = ts.reset();
+    ts.select(addr);
+    ts.write(0xBE); //Read ScratchPad
+
+    for (int i = 0; i < 9; i++) {
+        data[i] = ts.read();
+    }
+    ts.reset_search();
+
+    byte MSB = data[1];
+    byte LSB = data[0];
+
+    float temperatureRead = ((MSB << 8) | LSB); //Uses two's compliment
+    float celsius = temperatureRead / 16;
+    float fahrenheit = celsius * 1.8 + 32.0;
+
+    return celsius;
+    // Serial.print("  Temperature = ");
+    // Serial.print(celsius);
+    // Serial.print(" Celsius, ");
+    // Serial.print(fahrenheit);
+    // Serial.println(" Fahrenheit");
+}
+
+void attemptConnection() {
+    if (WiFi.status() == WL_NO_SHIELD) {
+        Serial.println("Wifi Shield not present");
+        //Discontinues the connection process
+        while (true);
+    }
+
+    //attempt to Connect to network
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid, pass);
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+
+    Serial.println("Connected to wifi");
+
+    printWiFiStatus();
+
+    Serial.println("\nStarting connection to server...");
+}
+
+void printWiFiStatus() {
+
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+}
+
+byte postJSON(char *domainBuffer, int domainPort, char *pageName, char *thisData) {
+    int inChar;
+    //Creates Buffer for Post Header
+    char outBuff[64];
+    int stringLength;
+
+    Serial.print(F("connecting...."));
+
+    if (client.connect(domainBuffer, domainPort) == 1) {
+        Serial.print(F("connected!!"));
+
+        //Sends Header
+        sprintf(outBuff, "POST %s HTTP/1.1", pageName);
+        client.println(outBuff);
+        sprintf(outBuff, "Host: %s", domainBuffer);
+        client.println(outBuff);
+        client.println(F("Connection: close\r\nContent-Type: application/json"));
+        client.println("User-Agent: Arduino Uno WiFi Rev2");
+        stringLength = strlen(thisData);
+        sprintf(outBuff, "Content-Length: %u\r\n", stringLength);
+        client.println(outBuff);
+
+
+        // send the body (variables)
+        client.print(thisData);
+    } else {
+        Serial.println(F("Failed to Send"));
+        return 0;
+    }
+
+    int connectLoop = 0;
+
+    while (client.connected()) {
+        while (client.available()) {
+            inChar = client.read();
+            Serial.write(inChar);
+            connectLoop = 0;
+        }
+        delay(1);
+        connectLoop++;
+
+        //Closes the connection if it exceeds the time limit
+        if (connectLoop > 10000) {
+            Serial.println();
+            Serial.println(F("Timeout"));
+            client.stop();
+        }
+    }
+    //Closes the connection after sending the data
+    Serial.println();
+    Serial.println(F("disconnecting."));
+    client.stop();
+    return 1;
+
+}
+
+void catJSON(float tempReading)
+{
+   float fahrenheit = tempReading * 1.8 + 32.0;
+   doc["Pool Sensor Data"] = 1;
+   JsonArray sensorInfo = doc.createNestedArray("Temperature");
+   JsonObject sensorData = sensorInfo.createNestedObject();
+
+   sensorData["Celsius"] = tempReading;
+   sensorData["Fahrenheit"] = fahrenheit;
+   serializeJsonPretty(doc, bodyBuff);
+   doc.garbageCollect();
+
+   if(!postJSON(serverName,serverPort,pageName,bodyBuff)) Serial.print(F("Fail "));
+   else Serial.print(F("Pass "));
 }
